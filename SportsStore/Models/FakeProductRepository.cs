dba79895c9eb20282c86fac14963b84c0821248d
Serg P-V <monsup@mail.ru>
2021-04-23 16:18:11 +0300

    Создание классов базы данных стр222

--- SportsStore/Models/FakeProductRepository.cs
+++ SportsStore/Models/FakeProductRepository.cs
@@ -10,7 +10,9 @@ namespace SportsStore.Models
         public IQueryable<Product> Products => new List<Product> {
             new Product{ Name="Football", Price = 25 },
             new Product{ Name="Surf board", Price = 179 },
-            new Product{ Name="Running shoes", Price = 95 }
+            new Product{ Name="Running shoes", Price = 95 },
+            new Product{ Name="Kimono Karate", Price = 24 },
+            new Product{ Name="Boots Soccers", Price = 75 }
         }.AsQueryable<Product>();
     }
 }
